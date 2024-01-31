a2fb4031841cd5ab134df5e43e541dfdb6dfb982
Juan Guerrero <JCystems@gMail.com>
2024-01-31 09:55:44 -0600

    Update run.ba.sh

--- scripts/docker/run.ba.sh
+++ scripts/docker/run.ba.sh
@@ -3,7 +3,7 @@
 set -e
 set -v
 
-docker run --network host -p 3798:3798 -t ${CONTAINER_REGISTRY_NAME}/${CONTAINER_IMAGE_NAME}:${CONTAINER_IMAGE_TAG}
+docker run --network host -p 3798:3798 -t ${CONTAINER_IMAGE_NAME}:${CONTAINER_IMAGE_TAG}
 
 set +v
 set +e
